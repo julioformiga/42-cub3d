09b7d7f7c0ccd583621835ab0ad01f0815501ec7
Julio Formiga <julio.formiga@gmail.com>
2025-04-19 15:31:45 +0200

    norm: player.c

--- src/player.c
+++ src/player.c
@@ -14,14 +14,11 @@
 
 static t_rect	rotate_rect(t_rect rect, t_point center, double angle)
 {
+	int		i;
 	t_rect	rotated_rect;
-	double	cos_angle;
-	double	sin_angle;
 	t_point	*src_points[4];
 	t_point	*dst_points[4];
 
-	cos_angle = cos(angle);
-	sin_angle = sin(angle);
 	src_points[0] = &rect.p0;
 	src_points[1] = &rect.p1;
 	src_points[2] = &rect.p2;
@@ -30,12 +27,13 @@ static t_rect	rotate_rect(t_rect rect, t_point center, double angle)
 	dst_points[1] = &rotated_rect.p1;
 	dst_points[2] = &rotated_rect.p2;
 	dst_points[3] = &rotated_rect.p3;
-	for (int i = 0; i < 4; i++)
+	i = -1;
+	while (i++, i < 4)
 	{
-		dst_points[i]->x = center.x + (src_points[i]->x - center.x) * cos_angle
-			- (src_points[i]->y - center.y) * sin_angle;
-		dst_points[i]->y = center.y + (src_points[i]->x - center.x) * sin_angle
-			+ (src_points[i]->y - center.y) * cos_angle;
+		dst_points[i]->x = center.x + (src_points[i]->x - center.x) * cos(angle)
+			+ (src_points[i]->y - center.y) * sin(angle);
+		dst_points[i]->y = center.y - (src_points[i]->x - center.x) * sin(angle)
+			+ (src_points[i]->y - center.y) * cos(angle);
 	}
 	return (rotated_rect);
 }
@@ -48,7 +46,6 @@ void	player(t_env *env)
 
 	x = env->map.player.x;
 	y = env->map.player.y;
-
 	rect = (t_rect){
 		(t_point){x - 20, y - 10},
 		(t_point){x - 20, y + 10},
@@ -58,6 +55,5 @@ void	player(t_env *env)
 	rect = rotate_rect(rect, (t_point){x, y}, env->map.player.direction);
 	ft_mlx_draw_rect(env, rect, BLUE, 1);
 	ft_mlx_draw_rect(env, rect, RED, 0);
-
 	raycasting(env);
 }
