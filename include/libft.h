4ab957193103dde00b02151c1018b403bc85d833
Julio Formiga <julio.formiga@gmail.com>
2025-05-08 17:00:05 +0200

    libft: add ft_isnumber

--- include/libft.h
+++ include/libft.h
@@ -38,6 +38,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_isnumber(char *number);
 size_t	ft_strlen(const char *str);
 void	*ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
---
+++ lib/libft/src/ft_isnumber.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isnumber.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julio.formiga <julio.formiga@gmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 16:55:55 by julio.formiga     #+#    #+#             */
+/*   Updated: 2025/05/08 16:55:55 by julio.formiga    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isnumber(char *number)
+{
+	int i;
+
+	i = -1;
+	while (i++, number[i])
+		if (!ft_isdigit(number[i]))
+			return (1);
+	return (0);
+}
+
